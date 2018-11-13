da596e2c3287bede324590f4d7bd8307b8ae24c1
Thiago Dias <repositorio.github.thiago.dias@gmail.com>
2018-11-13 10:45:21 -0200

    update iniciado dev teste unitario das classes

---
+++ src-test/br/com/iskuertow/prideus/util/DeleteDirTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.iskuertow.prideus.util;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Thiago
+ */
+public class DeleteDirTest {
+    
+    public DeleteDirTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of deleteDir method, of class DeleteDir.
+     */
+    @Test
+    public void testDeleteDir() {
+        System.out.println("deleteDir");
+        File dir = new File("casa");
+        boolean expResult = true;
+        boolean result = DeleteDir.deleteDir(dir);
+        assertEquals(expResult, result);
+    }
+    
+}
